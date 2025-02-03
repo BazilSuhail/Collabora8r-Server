9e921bb5ef73bb8f415c337dfb6de0e4cad5fbc7
BazilSuhail <bazil1854@gmail.com>
2025-02-03 22:46:51 +0500

    Deleted files

--- routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboard = require('../controllers/dashboardController');
-const authMiddleware  = require('../middleware/authMiddleware'); // Middleware to check token
-
-router.get('/assigned-tasks', authMiddleware, dashboard.fetchAssignedTasks);
-router.get('/progress-overview', authMiddleware, dashboard.fetchProgressOverview);
-
-module.exports = router;
